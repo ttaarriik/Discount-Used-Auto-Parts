a136bb785b540d5af095ded448378ca844ba44b7
Abdul Tareq Khaliq <ttaarriik1996@gmail.com>
2020-06-03 21:37:11 +0000

    Added dbs

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost/autoParts");
+mongoose.connect(process.env.DATABASEURL);
 
 
 
