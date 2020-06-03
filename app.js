cf9994c17cb4bc655b031b837992591f92e04dba
Abdul Tareq Khaliq <ttaarriik1996@gmail.com>
2020-06-03 21:33:07 +0000

    Changed port

--- app.js
+++ app.js
@@ -167,6 +167,6 @@ function isLoggedIn(req, res, next){
 
 
 
-app.listen(process.end.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("Server started");
 })
